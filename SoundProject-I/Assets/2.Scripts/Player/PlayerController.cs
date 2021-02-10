e0478323599794a70b144c3e8ff1b854a7f16a9d
Wesley <wesleyhokage123@outlook.com>
2021-02-10 17:37:54 -0300

    Player, Mechanics and sprites
    Changes in the texture properties of the sprites and the addition of the player with their respective animations and movement of the same also implemented

---
+++ SoundProject-I/Assets/2.Scripts/Player/PlayerController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D), typeof(Animator))]
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+
+    SpriteRenderer _sp;
+    Rigidbody2D _rb;
+    Animator _anim;
+
+    void Awake()
+    {
+        _anim = GetComponent<Animator>();
+        _rb = GetComponent<Rigidbody2D>();
+        _sp = GetComponent<SpriteRenderer>();
+    }
+
+    void Update() => OnMove(); 
+
+    void OnMove()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+
+        _rb.velocity = new Vector2(horizontal * Time.fixedDeltaTime * speed, _rb.velocity.y);
+        _anim.SetInteger("Move", (int)_rb.velocity.x);
+
+        if(horizontal > 0)
+            _sp.flipX = false;
+        else if(horizontal < 0)
+            _sp.flipX = true;
+
+    }
+}
